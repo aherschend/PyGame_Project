75ef5c9eaf2066bb2090f5b487c0b7550d01c9cb
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-10-11 15:57:50 -0500

    working

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
 
@@ -19,22 +20,41 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         #Set background color
         self.bg_color = (230, 230, 230)
 
     def run_game(self):
-        #start the main loop for the game
+        '''start the main loop for the game'''
         while True:
+            self._check_events()
+            self._update_screen()
             #Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+    def _check_events(self):
+        '''Respond to keypresses and mouse events'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+            
 
-            #Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
+    def _update_screen(self):
+        '''Update the images on the screen, and flip to the new screen'''
+        #Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            #Make the most recently drawn screen visible
-            pygame.display.flip()
+        #Make the most recently drawn screen visible
+        pygame.display.flip()
             
 if __name__ == '__main__':
     #Make a game instance, and run the gamev
