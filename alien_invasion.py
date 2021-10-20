d43c7a9911eeb6bdfcb789457c8976d92664abd1
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-10-20 10:38:10 -0500

    finished chapter 12

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,9 +25,10 @@ class AlienInvasion:
     def run_game(self):
         '''Start the main loop for the game.'''
         while True:
+            #This loop now has minimal code so we can see the method names and understand what's happening
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
     
     def _check_events(self):
@@ -60,8 +61,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         '''Create a new bullet and add it to the bullets group.'''
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        '''Update position of bullets and get rid of old bullets.'''
+        #Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have dissapeared (takes up too much processing power)
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
 
 
     
